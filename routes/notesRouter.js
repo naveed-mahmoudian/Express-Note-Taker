2c6dc7386394c7d92fc78974c181d428915857e2
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-26 14:58:43 -0700

    Fixed window location bug in index.html (client)

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const notesRouter = express.Router();
-// const db = require("../db/db.json");
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
