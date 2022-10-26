c76b5d55f48f5712e85f77eac2cf11bb21b8992e
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-26 13:30:34 -0700

    Added middleware to server.js

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const PORT = 3001;
+
+// Middleware
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Create server
+app.listen(PORT, () => {
+  console.log(`Server stared at http://localhost:${PORT}`);
+});
