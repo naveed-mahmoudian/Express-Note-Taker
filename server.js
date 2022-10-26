334b7c6c72abff14ac7a6664b5228b31adfb7d4e
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-26 15:46:39 -0700

    Updated port to use process.env in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const apiRouter = require("./routes/index");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.static("public"));
