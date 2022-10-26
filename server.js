c320500daeacb23cbbc774b82d8cfd68db16df9a
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-26 16:34:38 -0700

    renamed index.js in router folder to apiRouter.js (to reduce confusion)

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const apiRouter = require("./routes/index");
+const apiRouter = require("./routes/apiRouter");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
