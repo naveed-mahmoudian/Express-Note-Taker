f556461d55ecfbc19c53fcea58cb598b2d42e314
Naveed Mahmoudian <nmahmoudian@gmail.com>
2022-10-26 14:16:21 -0700

    Added api/notes/ POST functionality

--- routes/notesRouter.js
+++ routes/notesRouter.js
@@ -1,4 +1,47 @@
 const express = require("express");
 const notesRouter = express.Router();
+const db = require("../db/db.json");
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
+notesRouter.get("/", (req, res) => {
+  res.json(db);
+});
+
+notesRouter.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    fs.readFile("../db/db.json", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(newNote);
+        fs.writeFile("../db/db.json", JSON.stringify(parsedData), (err) => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log("Successfully wrote file");
+          }
+        });
+      }
+    });
+
+    const response = {
+      status: "Success",
+      body: newNote,
+    };
+
+    res.json(newNote);
+  } else {
+    res.status(500).json({ message: "Error saving note" });
+  }
+});
 module.exports = notesRouter;
